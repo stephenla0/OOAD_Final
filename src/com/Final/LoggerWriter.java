41419e49a9741691a39c276895743c099acb2e92
stephenla0 <00stephenlasko@gmail.com>
2022-05-01 18:44:23 -0600

    everything done

--- src/com/Final/LoggerWriter.java
+++ src/com/Final/LoggerWriter.java
@@ -1,10 +1,4 @@
 package com.Final;
 
 public interface LoggerWriter {
-    default void loggerOutln(String msg, Logger logger) {
-        logger.writeln(msg);
-    }
-    default void loggerOut(String msg, Logger logger){
-        logger.write(msg);
-    }
 }
